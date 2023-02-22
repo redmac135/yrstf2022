7d865e3f86f542b6c5992f5141b8dc78117aebad
redmac135 <ethanzhao1981@gmail.com>
2023-02-22 13:23:26 -0500

    update morse code print out

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ MORSE_TIME_FACTOR = 0.1
 AUDIO_FILE_PATH = "test.m4a"
 # 1 = audio
 # 2 = video
-CONFIG = 1 
+CONFIG = 1
 
 def clean_string(input_string: str) -> str:
     lowered = input_string.lower()
@@ -22,25 +22,24 @@ def clean_string(input_string: str) -> str:
     for pk, char in enumerate(split_string):
         if char not in ALLOWED_CHARS:
             split_string.pop(pk)
-    print(split_string)
     return "".join(split_string)
 
 def parse_morse(control: ArduinoControl, text: str) -> None:
-    cleanned = clean_string(text)
-    for char in cleanned:
+    for char in text:
         if char == " ":
             time.sleep(7*MORSE_TIME_FACTOR)
+            print(" ", end="")
         else:
             control.output_morse(morse[char])
             time.sleep(3*MORSE_TIME_FACTOR)
 
 if __name__ == "__main__":
+    control = ArduinoControl(MORSE_PIN, BRAILLE_START_PIN, MORSE_TIME_FACTOR)
     if CONFIG == 2:
         text = cameraInput()
-        print(text)
-        parse_morse(text)
     if CONFIG == 1:
         text = mp3_to_text(AUDIO_FILE_PATH)
-        control = ArduinoControl(MORSE_PIN, BRAILLE_START_PIN, MORSE_TIME_FACTOR)
-        print(text)
-        parse_morse(text)
+    cleanned = clean_string(text)
+    print("Raw Captured Text: " + text)
+    print("Cleanned Text: " + cleanned)
+    parse_morse(control, cleanned)
